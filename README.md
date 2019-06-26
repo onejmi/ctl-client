c8259de2b3b2b418f2df865a369385c714efe5a4
scarger <supremeterminal@gmail.com>
2019-06-26 16:51:54 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# CTL CLIENT
+Flutter (Web) application which interfaces with [ctl-server](http://github.com/scarger/ctl-server)
+
+### What's done?
+* Authentication page, interfaces with ctl-server's login route.
+
+### How to run
+Start the [ctl-server](http://github.com/scarger/ctl-server).
+Use `webdev serve` to debug the client.
+Add the `--auto restart` flag to enable hot reload.
---
+++ lib/routes/login.dart
@@ -0,0 +1,151 @@
+import 'package:flutter_web/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class LoginWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return LoginWidgetState();
+  }
+}
+
+class LoginWidgetState extends State<LoginWidget> {
+  final _formKey = GlobalKey<FormState>();
+
+  final emailFieldController = TextEditingController();
+  final passwordFieldController = TextEditingController();
+
+  bool isLoading = false;
+  bool hasTried = false;
+  String sessionID;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Material(
+        color: Colors.lightGreenAccent,
+        child: Center(
+          child: isLoading
+              ? const CircularProgressIndicator()
+              : SizedBox(
+                  height: 500,
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(50.0),
+                      child: Form(
+                        key: _formKey,
+                        child: Column(
+                          children: <Widget>[
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 15.0),
+                              child: Text(
+                                "Authenticate",
+                                style: TextStyle(
+                                  fontSize: 30,
+                                ),
+                              ),
+                            ),
+                            LoginField(
+                              "Email",
+                              Icons.email,
+                              (value) {
+                                if (value.isEmpty) {
+                                  return "Please enter an email";
+                                }
+                                return null;
+                              },
+                              emailFieldController,
+                            ),
+                            LoginField(
+                              "Password",
+                              Icons.security,
+                              (value) {
+                                if (value.isEmpty) {
+                                  return "Please enter a password";
+                                }
+                                return null;
+                              },
+                              passwordFieldController,
+                              isPassword: true,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 16.0),
+                              child: RaisedButton(
+                                color: Colors.lightBlueAccent,
+                                onPressed: () {
+                                  if (_formKey.currentState.validate()) {
+                                    login(emailFieldController.text,
+                                        passwordFieldController.text);
+                                    setState(() => isLoading = true);
+                                  }
+                                },
+                                child: Text("Login"),
+                              ),
+                            ),
+                            sessionID == null
+                                ? hasTried == false
+                                    ? null
+                                    : Padding(
+                                        padding: EdgeInsets.only(top: 15.0),
+                                        child: Text(
+                                          "Incorrect details",
+                                          style: TextStyle(color: Colors.red),
+                                        ))
+                                : Padding(
+                                    padding: EdgeInsets.only(top: 15.0),
+                                    child: Text(
+                                      "Success: $sessionID",
+                                      style: TextStyle(color: Colors.green),
+                                    ))
+                          ].where((widget) => widget != null).toList(),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> login(String email, String password) async {
+    final response = await http.post(
+      'http://localhost:8081/login',
+      body: json.encode({"email": email, "password": password}),
+    );
+    setState(() {
+      sessionID = json.decode(response.body)["session_id"];
+      isLoading = false;
+      hasTried = true;
+    });
+  }
+}
+
+class LoginField extends StatelessWidget {
+  final _labelText;
+  final IconData _iconData;
+  final bool isPassword;
+  final _validator;
+  final _controller;
+
+  const LoginField(
+      this._labelText, this._iconData, this._validator, this._controller,
+      {this.isPassword = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 300,
+      child: TextFormField(
+        controller: _controller,
+        obscureText: isPassword,
+        decoration: InputDecoration(
+          labelText: _labelText,
+          icon: Icon(_iconData),
+        ),
+        validator: _validator,
+      ),
+    );
+  }
+}
